b040e4040f261f2f03ae76a70fb7cd5c9377910f
yang.zisong <yang.zisong@embedway.com>
2018-10-10 18:20:22 +0800

    写入部分优先级更新代码

--- src/main/acs_run.java
+++ src/main/acs_run.java
@@ -53,8 +53,73 @@ public class acs_run {
 	{
 		for (Teaching t:teaching_list)
 		{
-			
+
 		}
+	}
+
+	private double []  get_priorities(Teaching teaching)
+	{
+		int	class_list[] = teaching.get_class_list();
+		int	time_list[] = teaching.get_time_lsit();
+		int	cl_num = class_list.length - 1;
+		int	class_needed_periods[] = new int[cl_num + 1];
+		int	class_available_periods[] = new int[cl_num + 1];
+		int	class_arrangment_count[] = new int[cl_num + 1];
+		int	period_arrangment[] = new int[total_periods + 1];
+		int t_id = teaching.get_teacher().get_id();
+		int periods = teaching.get_course().get_periods();
+
+		double priorities[] = new double[cl_num + 2];
+
+		for¡¡(int i= 1;i <= cl_num; i++)
+		{
+
+			for ( int j = 1;j <= total_periods; j ++)
+			{
+				if (timetable[i][j] == t_id)
+				{
+					class_arrangment_count[i] ++;
+					period_arrangment[j] = 1;
+				}
+			}
+		}
+
+		boolean is_computer_course = false;
+		if ( teaching.get_course().get_short_name().equals("µç"))
+			is_computer_course = true;
+
+		for ( int i = 1; i <= cl_num; i ++)
+		{
+			class_available_periods[i] = get_available_periods(i, period_arrangment, time_list, is_computer_course);
+			class_needed_periods[i] = periods - class_arrangment_count[i];
+
+			priorities[i] = double(class_needed_periods[i])/(double)(class_available_periods[i]);
+
+		}
+
+	}
+
+	private int get_available_periods(int c_no, int period_arrangment[], int time_list[], boolean is_computer_course)
+	{
+		int count = 0;
+		boolean effect_flag;
+		for ( int j = 1;j <= total_periods; j ++)
+		{
+			if (period_arrangment[j] == 0 && time_list[j] == 1 && timetable[c_no][j] ==0)
+				effect_flag = true;
+			else
+				effect_flag = false;
+
+			if (is_computer_course){
+					if ( effect_flag && computer_room_used[j] <= computer_room_num)
+							count ++;
+			}else{
+				if (effect_flag)
+					count ++;
+			}
+
+		}
+		return count;
 	}
 
 }
